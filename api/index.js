86f0a1f7cfefe3aead239491c25972d35040488d
Pjmaciel <pjmaciel_4@hotmail.com>
2023-10-05 23:07:10 -0300

    Config db freemysqlhosting

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3306;
 
 routes(app);
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
